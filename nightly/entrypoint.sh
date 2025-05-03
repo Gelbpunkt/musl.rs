1fd8a875371ba4f35c3813ad71d8d0cded6e3c68
Jens Reidel <adrian@travitia.xyz>
2025-05-04 01:07:44 +0200

    Update nightly patches
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- nightly/entrypoint.sh
+++ nightly/entrypoint.sh
@@ -18,23 +18,22 @@ for patch in /src/nightly/*.patch; do
 	patch -p1 < $patch
 done
 
-/src/clear_checksums.py vendor/libc-0.2.107/.cargo-checksum.json
-/src/clear_checksums.py vendor/libc-0.2.112/.cargo-checksum.json
-/src/clear_checksums.py vendor/libc-0.2.119/.cargo-checksum.json
-/src/clear_checksums.py vendor/libc-0.2.121/.cargo-checksum.json
-/src/clear_checksums.py vendor/libc-0.2.124/.cargo-checksum.json
-/src/clear_checksums.py vendor/libc-0.2.155/.cargo-checksum.json
-/src/clear_checksums.py vendor/libc-0.2.167/.cargo-checksum.json
-/src/clear_checksums.py vendor/libc-0.2.168/.cargo-checksum.json
-/src/clear_checksums.py vendor/libc-0.2.169/.cargo-checksum.json
-/src/clear_checksums.py vendor/libc-0.2.170/.cargo-checksum.json
-/src/clear_checksums.py vendor/libc-0.2.171/.cargo-checksum.json
-/src/clear_checksums.py vendor/libc-0.2.94/.cargo-checksum.json
 /src/clear_checksums.py vendor/libc-0.2.97/.cargo-checksum.json
-/src/clear_checksums.py vendor/compiler_builtins-0.1.151/.cargo-checksum.json
-/src/clear_checksums.py vendor/libffi-sys-2.3.0/.cargo-checksum.json
+/src/clear_checksums.py vendor/libc-0.2.94/.cargo-checksum.json
+/src/clear_checksums.py vendor/libc-0.2.172/.cargo-checksum.json
+/src/clear_checksums.py vendor/libc-0.2.171/.cargo-checksum.json
+/src/clear_checksums.py vendor/libc-0.2.170/.cargo-checksum.json
+/src/clear_checksums.py vendor/libc-0.2.169/.cargo-checksum.json
+/src/clear_checksums.py vendor/libc-0.2.168/.cargo-checksum.json
+/src/clear_checksums.py vendor/libc-0.2.155/.cargo-checksum.json
+/src/clear_checksums.py vendor/libc-0.2.124/.cargo-checksum.json
+/src/clear_checksums.py vendor/libc-0.2.121/.cargo-checksum.json
+/src/clear_checksums.py vendor/libc-0.2.119/.cargo-checksum.json
+/src/clear_checksums.py vendor/libc-0.2.112/.cargo-checksum.json
+/src/clear_checksums.py vendor/libc-0.2.107/.cargo-checksum.json
+/src/clear_checksums.py vendor/compiler_builtins-0.1.156/.cargo-checksum.json
 
-cp /src/nightly/config.toml .
+cp /src/nightly/bootstrap.toml .
 
 ./x.py dist --jobs $(nproc)
 
